7f2f8fd62bf8b65aed8989ffa5258b9c6ad13030
Jan Hoffmann <jan@3e8.eu>
2026-03-01 00:50:59 +0100

    Restructure code as preparation for encryption script
    Move common code to a new file. While at it, make use of the walrus
    operator for make it easier to read.

---
+++ common.py
@@ -0,0 +1,71 @@
+import hashlib
+
+
+# header format: length 0x140
+# offset 0x20, length 0x20: AES256-CBC IV
+# offset 0x40, length 0x20: PBKDF2 salt
+# offset 0x60, length 0x20: encrypted file size (encoded as string)
+# offset 0x80, length 0x20: CRC32 of entire file with zeroed CRC field (encoded as string)
+# offset 0xa0, length 0x20: unencrypted payload size (encoded as string)
+# offset 0xc0, length 0x2a: magic string
+
+
+HEAD_SIZE = 0x140
+
+OFFSET_IV = 0x20
+OFFSET_SALT = 0x40
+OFFSET_FILE_SIZE = 0x60
+OFFSET_CRC32 = 0x80
+OFFSET_PAYLOAD_SIZE = 0xa0
+OFFSET_MAGIC = 0xc0
+
+LENGTH_HEAD_FIELD_DEFAULT = 0x20
+LENGTH_HEAD_FIELD_MAGIC = 0x2a
+
+
+PUBLIC_KEY = \
+	b"-----BEGIN PUBLIC KEY-----\n" + \
+	b"MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArqEZkeCLVN+smJ0C4WFH\n" + \
+	b"bH6OkgDY7XXywRkhsyqIDJ2HAGaaDQ4uMPgqyIvUnGY08XOXi0ptlX73ll+6qlxr\n" + \
+	b"hhFZnMfCguhUgILrydqSMZmGjwbU1TZJIC9eM/QOQUAbvwUP9GsxOriEAK3/FpHQ\n" + \
+	b"sz7tZXXv7zzrn0oKba7YHsX8o5HUR5nB6gQQdDpkqWOdcIkiaRKSODnjS19zCzqi\n" + \
+	b"l/mZ5OPOu+vAbF32IbhzcRu++VrHJ1PpbGNCK+Xj6Se+h4OWgnuGkcrMJNHh1Smi\n" + \
+	b"yANXFyqekMIVBtgXdgez1x6xuoQKY4cDZJMGiM14eanhZQSImgIYwF8ijbxjW+se\n" + \
+	b"bQIDAQAB\n" + \
+	b"-----END PUBLIC KEY-----\n"
+
+MAGIC = b"534320200402aaa567787970746562366c6dsc5343"
+
+
+def get_head_field(buf, offset):
+	length = LENGTH_HEAD_FIELD_MAGIC if offset == OFFSET_MAGIC else LENGTH_HEAD_FIELD_DEFAULT
+
+	return buf[offset:offset+length]
+
+
+def set_head_field(buf, offset, val):
+	length = LENGTH_HEAD_FIELD_MAGIC if offset == OFFSET_MAGIC else LENGTH_HEAD_FIELD_DEFAULT
+
+	if length != len(val):
+		raise Exception("incorrect header field length")
+
+	buf[offset:offset+length] = val
+
+
+def decode_int(val):
+	return int(val.rstrip(b"\x00"))
+
+
+def encode_int(val):
+	string = str(val)
+	padding = b"\x00" * (LENGTH_HEAD_FIELD_DEFAULT - len(string))
+	return bytes(string, "ascii") + padding
+
+
+def calculate_aes_key(salt, payload_size):
+	password = bytearray(0x200)
+	password[0:0x100] = PUBLIC_KEY[0:0x100]
+	password[0x100:0x12a] = MAGIC
+	password[0x12a:0x14a] = encode_int(payload_size)
+
+	return hashlib.pbkdf2_hmac("sha256", password, salt, 1000)
--- decrypt.py
+++ decrypt.py
@@ -3,43 +3,21 @@
 import sys
 import os
 import binascii
-import hashlib
 from Crypto.Cipher import AES
 
-
-# header format: length 0x140
-# offset 0x20, length 0x20: AES256-CBC IV
-# offset 0x40, length 0x20: PBKDF2 salt
-# offset 0x60, length 0x20: encrypted file size (encoded as string)
-# offset 0x80, length 0x20: CRC32 of entire file with zeroed CRC field (encoded as string)
-# offset 0xa0, length 0x20: unencrypted payload size (encoded as string)
-# offset 0xc0, length 0x2a: magic string
-
-
-HEAD_SIZE = 0x140
-
-PUBLIC_KEY = \
-	b"-----BEGIN PUBLIC KEY-----\n" + \
-	b"MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArqEZkeCLVN+smJ0C4WFH\n" + \
-	b"bH6OkgDY7XXywRkhsyqIDJ2HAGaaDQ4uMPgqyIvUnGY08XOXi0ptlX73ll+6qlxr\n" + \
-	b"hhFZnMfCguhUgILrydqSMZmGjwbU1TZJIC9eM/QOQUAbvwUP9GsxOriEAK3/FpHQ\n" + \
-	b"sz7tZXXv7zzrn0oKba7YHsX8o5HUR5nB6gQQdDpkqWOdcIkiaRKSODnjS19zCzqi\n" + \
-	b"l/mZ5OPOu+vAbF32IbhzcRu++VrHJ1PpbGNCK+Xj6Se+h4OWgnuGkcrMJNHh1Smi\n" + \
-	b"yANXFyqekMIVBtgXdgez1x6xuoQKY4cDZJMGiM14eanhZQSImgIYwF8ijbxjW+se\n" + \
-	b"bQIDAQAB\n" + \
-	b"-----END PUBLIC KEY-----\n"
-
-MAGIC = b"534320200402aaa567787970746562366c6dsc5343"
+from common import *
 
 
 def calculate_crc32(file):
 	file.seek(0)
-	crc = 0
+
 	buf = bytearray(file.read(HEAD_SIZE))
-	buf[0x80:0xa0] = 0x20 * b"\x00"
-	while buf:
+	set_head_field(buf, OFFSET_CRC32, 0x20 * b"\x00")
+	crc = binascii.crc32(buf)
+
+	while buf := file.read(1024):
 		crc = binascii.crc32(buf, crc)
-		buf = file.read(1024)
+
 	return crc
 
 
@@ -48,66 +26,51 @@ def read_header(file):
 	if len(head) != HEAD_SIZE:
 		raise Exception("failed to read header")
 
-	magic = head[0xc0:0xc0+len(MAGIC)]
+	magic = get_head_field(head, OFFSET_MAGIC)
 	if magic != MAGIC:
 		raise Exception("unexpected magic value at offset 0xc0")
 
-	size = head[0x60:0x80]
-	size_int = int(size.rstrip(b"\x00"))
+	size = decode_int(get_head_field(head, OFFSET_FILE_SIZE))
 
 	file.seek(0, os.SEEK_END)
 	actual_size = file.tell()
 
-	if size_int != actual_size:
-		raise Exception(f"unexpected file size ({size_int} != {actual_size})")
+	if size != actual_size:
+		raise Exception(f"unexpected file size ({size} != {actual_size})")
 
-	crc = head[0x80:0xa0]
-	crc_uint = int(crc.rstrip(b"\x00"))
+	crc = decode_int(get_head_field(head, OFFSET_CRC32))
 
 	actual_crc = calculate_crc32(file)
 
-	if crc_uint != actual_crc:
-		raise Exception(f"unexpected CRC32 ({crc_uint} != {actual_crc})")
+	if crc != actual_crc:
+		raise Exception(f"unexpected CRC32 ({crc} != {actual_crc})")
 
-	iv = head[0x20:0x40]
-	salt = head[0x40:0x60]
-	payload_size = head[0xa0:0xc0]
+	iv = get_head_field(head, OFFSET_IV)
+	salt = get_head_field(head, OFFSET_SALT)
+	payload_size = decode_int(get_head_field(head, OFFSET_PAYLOAD_SIZE))
 
-	payload_size_int = int(payload_size.rstrip(b"\x00"))
-
-	return iv, salt, payload_size, payload_size_int
-
-
-def calculate_aes_key(salt, size):
-	password = bytearray(0x200)
-	password[0:0x100] = PUBLIC_KEY[0:0x100]
-	password[0x100:0x100+len(MAGIC)] = MAGIC
-	password[0x12a:0x14a] = size
-
-	return hashlib.pbkdf2_hmac("sha256", password, salt, 1000)
+	return iv, salt, payload_size
 
 
 def decrypt_image(file_in, file_out, key, iv):
 	cipher = AES.new(key, AES.MODE_CBC, iv=iv[0:16])
 
 	file_in.seek(HEAD_SIZE)
-	buf = file_in.read(1024)
-	while buf:
+	while buf := file_in.read(1024):
 		out = cipher.decrypt(buf)
 		file_out.write(out)
-		buf = file_in.read(1024)
 
 
 def main(filename_in, filename_out):
 	file_in = open(filename_in, "rb")
 
-	iv, salt, size, size_int = read_header(file_in)
-	key = calculate_aes_key(salt, size)
+	iv, salt, payload_size = read_header(file_in)
+	key = calculate_aes_key(salt, payload_size)
 
 	file_out = open(filename_out, "wb")
 
 	decrypt_image(file_in, file_out, key, iv)
-	file_out.truncate(size_int)
+	file_out.truncate(payload_size)
 
 	file_in.close()
 	file_out.close()
